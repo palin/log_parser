a22144a3353f352c1becd607477acc42ad156242
Maciej Palinski <c_mpalinski@groupon.com>
2022-08-27 12:37:32 +0200

    extract smaller entities

--- log_parser/core.rb
+++ log_parser/core.rb
@@ -1,6 +1,36 @@
 # frozen_string_literal: true
 
 module LogParser
+  LogLine = Struct.new(:line) do
+    def page
+      line.strip.split(' ')[0]
+    end
+
+    def ip
+      line.strip.split(' ')[1]
+    end
+  end
+
+  # Helper class for storing single page data from logs
+  class SinglePageInfo
+    attr_reader :summary
+
+    def initialize
+      @summary = { regular: 0, unique: 0, unique_ips: [] }
+    end
+
+    def add_regular_visit
+      @summary[:regular] += 1
+    end
+
+    def add_unique_visit(ip)
+      return if @summary[:unique_ips].include?(ip)
+
+      @summary[:unique] += 1
+      @summary[:unique_ips] << ip
+    end
+  end
+
   # Parses a log file and returns a hash
   module Core
     def self.run(filename)
@@ -12,21 +42,10 @@ module LogParser
       results = {}
 
       File.readlines(filename, chomp: true).each do |line|
-        result = line.strip.split(' ')
-        page = result[0]
-        ip = result[1]
-        results[page] ||= { regular: 0, unique: 0, unique_ips: [] }
-        regular = results[page][:regular] + 1
-
-        if results[page][:unique_ips].include?(ip)
-          unique = results[page][:unique]
-          unique_ips = results[page][:unique_ips]
-        else
-          unique = results[page][:unique] + 1
-          unique_ips = results[page][:unique_ips] << ip
-        end
-
-        results[page] = { regular:, unique:, unique_ips: }
+        log_line = LogLine.new(line)
+        results[log_line.page] ||= SinglePageInfo.new
+        results[log_line.page].add_regular_visit
+        results[log_line.page].add_unique_visit(log_line.ip)
       end
 
       results
@@ -40,11 +59,11 @@ module LogParser
     end
 
     def self.sort_results_for_type(results, type)
-      results.sort_by { |_k, v| v[type] }.reverse!
+      results.sort_by { |_k, v| v.summary[type] }.reverse!
     end
 
     def self.build_results_for_type(results, type)
-      sort_results_for_type(results, type).map { |r| [r.first, r.last[type]] }
+      sort_results_for_type(results, type).map { |r| [r.first, r.last.summary[type]] }
     end
   end
 end
